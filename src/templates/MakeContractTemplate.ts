48a88d3a2556ad3f597b72385c5fb6795f13a7b9
Levy Santiago <levyssantiago@gmail.com>
2022-03-27 17:34:09 -0300

    updating contract creation template

--- src/templates/MakeContractTemplate.ts
+++ src/templates/MakeContractTemplate.ts
@@ -1,3 +1,17 @@
 //Contract template
-class Contract {}
-export default Contract;
+// Factory
+import { createContract } from "gifflar";
+
+// Creating contract model
+const myContract = createContract("MyContract");
+
+// Creating a contract variable
+myContract.createVariable("string", "message", "public");
+
+// Creating a contract constructor function
+myContract
+  .createConstructor("public")
+  .setInput("string", "_message")
+  .setAssignment("message", "_message");
+
+export default myContract;
