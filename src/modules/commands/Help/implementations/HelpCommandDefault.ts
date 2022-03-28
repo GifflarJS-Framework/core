543e18e69a44ada9c14ad7e2c0c1c9bd2d0a9733
Levy Santiago <levyssantiago@gmail.com>
2022-03-28 18:39:35 -0300

    configuring help command output

--- src/modules/commands/Help/implementations/HelpCommandDefault.ts
+++ src/modules/commands/Help/implementations/HelpCommandDefault.ts
@@ -2,7 +2,20 @@ import { IHelpCommand } from "../types/IHelpCommand";
 
 class HelpCommandDefault implements IHelpCommand {
   async execute(value: string): Promise<void> {
-    console.log("Help");
+    console.log("Usage:");
+    console.log(" command [arguments] [options]");
+
+    console.log("\nAvailable Commands");
+    console.log(
+      " help, --help, -h                           \t Show all the Gifflar available commands."
+    );
+    console.log(
+      " init, --init                               \t Initializes the Gifflar configuration file."
+    );
+    console.log(" make");
+    console.log(
+      "  make:contract, --make:contract, -m:contract \t Make a new Gifflar Contract. Required a file name as argument."
+    );
   }
 }
 export default HelpCommandDefault;
